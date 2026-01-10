82e83abcc0a36e475156d019505ba807df2c9abb
saba <sarzoo756@gmail.com>
2026-01-10 13:14:43 +0530

    removed old

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,4 @@
 import { Routes } from '@angular/router';
-import { Topic1BasicsComponent } from './components/topic1-basics/topic1-basics.component';
-import { Topic1ParentComponent } from './components/topic1-parent/topic1-parent.component';
-import { Topic3ObservableBasicComponent } from './components/topic3-observable-basic/topic3-observable-basic.component';
-import { CounterParentComponent } from './signals-lab/counter-parent/counter-parent.component';
-import { LifecyclePracticeComponent } from './lifecycle-practice/lifecycle-practice.component';
 export const routes: Routes = [
-  {
-    path: 'topic1-Component',
-    component: Topic1BasicsComponent
-  },
-  {
-    path: 'parent-child-communication',
-    component: Topic1ParentComponent
-  },
-  { path: 'observable-basic', component: Topic3ObservableBasicComponent },
-  { path: 'signals', component: CounterParentComponent },
-  { path: 'lifecycle-hooks', component: LifecyclePracticeComponent },
+  
 ];
